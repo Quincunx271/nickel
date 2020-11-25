da9d967c537c202f66088a88cfec86c59a1ca3a2
Justin Bassett <jbassett271@gmail.com>
2020-11-24 22:25:24 -0800

    Add memory use benchmark visualization

--- benchmarks/visualize.py
+++ benchmarks/visualize.py
@@ -22,3 +22,13 @@ if __name__ == '__main__':
 
         plt.legend()
         plt.show()
+
+        plt.ylabel('Memory (MB)')
+        plt.xlabel('N')
+        plt.title(benchmark + ' memory')
+
+        for which, which_results in bench_results.items():
+            plt.plot([x['n'] for x in which_results['results']], [(x['memory'] - x['baseline']['memory']) / 1e3 for x in which_results['results']], label=which_results['which'])
+
+        plt.legend()
+        plt.show()
