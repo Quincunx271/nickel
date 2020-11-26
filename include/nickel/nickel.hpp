98a601b8c3636932654648546b9a69ec8d2abfad
Justin Bassett <jbassett271@gmail.com>
2020-11-25 20:52:50 -0800

    Add Boost license

--- include/nickel/nickel.hpp
+++ include/nickel/nickel.hpp
@@ -1,3 +1,8 @@
+//          Copyright Justin Bassett 2019 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <type_traits>
