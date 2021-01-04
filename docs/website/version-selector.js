f9685cf1607822553cca9d7776d77574c3789599
Justin Bassett <jbassett271@gmail.com>
2021-01-04 11:24:55 -0800

    📝 Add banner for outdated version

--- docs/website/version-selector.js
+++ docs/website/version-selector.js
@@ -4,6 +4,21 @@
     // Replaced by a script
     const versions = []
 
+    function createOutdatedBanner(latestVersion, currentVersion) {
+        if (latestVersion === currentVersion || currentVersion === versions[0][0]) {
+            return ''
+        }
+
+        const root = $('body').data('documentation-root')
+        const page = $('body').data('documentation-current-page')
+        var versionBase = `${root}/${versions[1][0]}`
+        var redirectUrl = `${versionBase}/${page}`
+        return `<div class='body' id='outdated-banner' style='background-color: khaki; text-align: center; padding: 5px; max-width: none'>
+            This is documentation for an older version of Nickel.
+            <a href='${redirectUrl}'>Click here to go to the latest version.</a>
+        </div>`
+    }
+
     function createSelector(currentVersion) {
         var generated = ['<select id="version-selector">']
 
@@ -25,10 +40,17 @@
         const page = $('body').data('documentation-current-page')
         const versionRoot = window.location.pathname.slice(0, -page.length - 1) // extra char == last '/'
         const version = versionRoot.substring(versionRoot.lastIndexOf('/') + 1)
-        var selectorHtml = createSelector(version)
+        const selectorHtml = createSelector(version)
 
-        var selectorContainer = document.getElementById('version-selector-container');
+        const selectorContainer = document.getElementById('version-selector-container');
         selectorContainer.innerHTML = selectorHtml
+
+        const outdatedBannerHtml = createOutdatedBanner(versions[1][0], version)
+        if (outdatedBannerHtml != '') {
+            const docDiv = document.querySelector('div.document')
+            const banner = new DOMParser().parseFromString(outdatedBannerHtml, 'text/html').body.firstChild
+            docDiv.parentElement.insertBefore(banner, docDiv)
+        }
 
         $(selectorContainer).ready(function () {
             var selector = document.getElementById('version-selector')
